6e95c87d50908bc214c769b4e359e5c945091688
Pooja-chaudhary960 <pujadahit52@gmail.com>
2026-06-04 15:38:24 +0545

    Day-2 of Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Navbar from './Components/Navbar';
+import ProductList from './pages/ProductList';
+import Cart from './Pages/Cart';
+import Checkout from './pages/Checkout';
+import ProductDetails from './pages/ProductDetail.jsx';
+import Footer from './Components/Footer';
 
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+const App = () => {
   return (
-    <div className='text-2xl'>App</div>
+   <>
+     <Router>
+      <div className="min-h-screen bg-gray-950 font-sans">
+        <Navbar />
+       <Routes>
+         <Route path="/" element={<ProductList />} /> 
+         <Route path="/product/:id" element={<ProductDetails />} />
+         <Route path="/cart" element={<Cart />} />
+       </Routes>
+       <Footer />
+       </div>
+     </Router>
+   </>
   )
 }
 
