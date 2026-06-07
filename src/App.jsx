15e961d3df38582fa433ae7e4b0d0523311a6ffe
Pooja-chaudhary960 <pujadahit52@gmail.com>
2026-06-07 15:22:11 +0545

    Work on Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import React from 'react'
+import React from 'react';
 import Navbar from './Components/Navbar';
 import ProductList from './pages/ProductList';
-import Cart from './Pages/Cart';
+import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
-import ProductDetails from './pages/ProductDetail.jsx';
+import ProductDetails from './pages/ProductDetail';
 import Footer from './Components/Footer';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-   <>
-     <Router>
+    <Router>
       <div className="min-h-screen bg-gray-950 font-sans">
         <Navbar />
-       <Routes>
-         <Route path="/" element={<ProductList />} /> 
-         <Route path="/product/:id" element={<ProductDetails />} />
-         <Route path="/cart" element={<Cart />} />
-       </Routes>
-       <Footer />
-       </div>
-     </Router>
-   </>
-  )
-}
 
-export default App
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
